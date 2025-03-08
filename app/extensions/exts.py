8086533fdb05e9018737cf29ef08ddf5de8ee58f
Bingxin <73421892+bingxin666@users.noreply.github.com>
2025-03-09 00:19:34 +0800

    测试添加音频和歌词支持

--- app/extensions/exts.py
+++ app/extensions/exts.py
@@ -1,9 +1,9 @@
 from typing import Final
 
-VIDEO_EXTS: Final = frozenset((".mp4", ".mkv", ".flv", ".avi", ".wmv", ".ts", ".rmvb", ".webm", "wmv"))
+VIDEO_EXTS: Final = frozenset((".m4a", "mp3", ".wav", ".flac", ".mp4", ".mkv", ".flv", ".avi", ".wmv", ".ts", ".rmvb", ".webm", "wmv"))
 EXTENDED_VIDEO_EXTS: Final = VIDEO_EXTS.union((".strm",))
 
-SUBTITLE_EXTS: Final = frozenset((".ass", ".srt", ".ssa", ".sub"))
+SUBTITLE_EXTS: Final = frozenset(("lrc", ".ass", ".srt", ".ssa", ".sub"))
 
 IMAGE_EXTS: Final = frozenset((".png", ".jpg"))
 
